77ce2180b4e31249ea0113369aa31ee70a0d0e93
AmadorZcv <lucasamadorzcv@gmail.com>
2020-02-12 00:07:06 -0300

    :sparkles: showing champions faces

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from "react";
 import fogged from "./data/fogged.json";
 import "./App.css";
-
+import { championImage } from "./data/champions";
+const capitalize = s => {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
+//console.log(championImage("aatrox"));
+function fodac() {
+  const array = fogged.reduce((atual, value) => {
+    return (
+      atual +
+      `const ${value.champion
+        .toLowerCase()
+        .replace(/[\s'.]/g, "")} = require("../img/champion/${capitalize(
+        value.champion.replace(/[\s'.]/g, "").toLowerCase()
+      )}.png");`
+    );
+  }, "");
+  console.log(array);
+}
 function App() {
   console.log(fogged);
+  console.log(fodac());
   return (
     <div className="App">
       <li>
         {fogged.map(value => (
-          <ul>{value.champion}</ul>
+          <div>
+            <ul>{value.champion}</ul>
+            <img
+              src={championImage(
+                value.champion.toLowerCase().replace(/[\s'.]/g, "")
+              )}
+            />
+          </div>
         ))}
       </li>
     </div>
